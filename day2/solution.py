169e6d20fe34c1dfa5d941f6a79b67027f0c516c
Joana Teodoro <joana.teodoro@codacy.com>
2023-12-14 14:22:53 +0000

    add part 2

--- day2/solution.py
+++ day2/solution.py
@@ -5,6 +5,7 @@ def readInput():
 
 def processLine(line, id):
     line = line.replace('Game ' + str(id) + ': ', '')
+    line = line.replace('\n', '')
     line = line.replace(' ', '')
     line = line.replace(';', ',')
     line = line.split(',')
@@ -12,8 +13,6 @@ def processLine(line, id):
 
 def discardImpossibleGameSets(lines):
     ids = []
-    number = 0
-    color = ''
     for l in range(len(lines)):
         id = l+1
         line = lines[l]
@@ -33,11 +32,34 @@ def discardImpossibleGameSets(lines):
             ids.append(id)
     return ids
 
+def computeLowestNumberOfCubes(line, id):
+    lowestPossibleSet = {'red': 0, 'green': 0, 'blue': 0}
+    processedLine = processLine(line, id)
+    for part in processedLine:
+        currentNumber = int(''.join(filter(str.isdigit, part)))
+        currentColor = part.replace(str(currentNumber), '')
+        if(currentNumber > lowestPossibleSet[currentColor]):
+            lowestPossibleSet[currentColor] = currentNumber
+    return lowestPossibleSet
+
+def computePower(set):
+    return set['red'] * set['green'] * set['blue']
+
+
 def partOne():
     lines = readInput()
-    #lines = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
     ids = discardImpossibleGameSets(lines)
     return sum(ids)
 
+def partTwo():
+    lines = readInput()
+    totalPower = 0
+    for l in range(len(lines)):
+        id = l+1
+        line = lines[l]
+        currentSet = computeLowestNumberOfCubes(line, id)
+        totalPower += computePower(currentSet)
+    return totalPower
 
 print(partOne())
+print(partTwo())
